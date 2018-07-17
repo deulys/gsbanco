dcc1a053ad39d3a1a14410e7239014c98b3ce7e5
ultraton <ultraton@DESKTOP-DUMS2UD>
2018-07-17 02:32:57 -0300

    PDO error

--- public_html/controller/gsbancoController.php
+++ public_html/controller/gsbancoController.php
@@ -3,6 +3,7 @@
 Class gsbanco {
 
     static $_instance;
+    public $campo=array();
 
     public function __construct() {
         /* Incluimos el fichero de la clase Db */
@@ -34,6 +35,57 @@ Class gsbanco {
         
         //$this->enviarcorreo1($array);
     }
+    
+    public function gsbancoUpdateCuentaController($campo) {
+        $this->campo=$campo;
+        //var_dump($this->campo);
+        $db = new \PDOWrapper\DB;
+        
+        ($db->bindMore(array("idcuenta"=>$this->campo['cuenta_id'])));
+        $estado_conciliacion = $db->query("SELECT * FROM conciliacion where idcuenta= :idcuenta");
+        
+        $update   =  $db->query("UPDATE cuentas SET activa = :activa,saldoinicialint=:monto_libro,saldoinicialext=:monto_banco WHERE id = :idcuenta", array("activa"=>"1","monto_libro"=>$this->campo['monto_libro'],"monto_banco"=>$this->campo['monto_banco'],"idcuenta"=>$this->campo['cuenta_id']));
+        
+        
+        //////comprueba que exista la conciliacion si no existe la inserta y le cambia estado a cerrada
+        
+        $estado_conciliacion=count($db->query("SELECT id FROM conciliacion where idcuenta= :idcuenta"));
+      
+        var_dump($campo);
+        if ($estado_conciliacion>0){
+            $db1 = new \PDOWrapper\DB;
+            var_dump($this->campo);
+            ($db1->bindMore(array("idcuenta"=>$this->campo['cuenta_id'])));
+            $update   =  $db1->query("UPDATE conciliacion SET idestado = :idestado WHERE idcuenta = :idcuenta", array("idestado"=>"3","idcuenta"=>$this->campo['cuenta_id']));
+            
+        }else{
+            $db1 = new \PDOWrapper\DB;
+            ($db1->bindMore(array("idcuenta"=>$this->campo['cuenta_id'])));
+            $insert   =  $db1->query("INSERT INTO conciliacion (idcuenta,idestado) VALUES(:idcuenta,:idestado)", array("idcuenta"=>$this->campos["cuenta_id"],"idestado"=>'3'));
+        }
+       
+        //se vuelve a verificar que exista la concilion para actualiza el estatus de la cuenta       
+       
+        if($update > 0 ) {
+            //return 'Succesfully created a new person !';
+        }
+        
+       
+      
+        ///////Envio de mail
+//        if($stmt==true){
+//        $resultado=array("mensaje"=>'mensaje',"tipo_mensaje"=>4,"mensaje_pantalla"=>"Su Pago Sera Realizado a la Brevedad","tiempo"=>4000);
+//            $this->enviarcorreo1($array);
+//            return $resultado;
+//            
+//        }else{
+//            $resultado=array("mensaje"=>'mensaje',"tipo_mensaje"=>3,"mensaje_pantalla"=>"Ha Ocurrido un Error Intente cargar su Venta Nuevamente","tiempo"=>4000);
+//            return $resultado;
+//        }
+        
+        //$this->enviarcorreo1($array);
+    }
+    
    public function key_implode($array, $glue) {
     $result = "";
     foreach ($array as $key => $value) {
@@ -44,6 +96,7 @@ Class gsbanco {
 }
     
     public function gsbancoGetMovimientoController($campos,$condicion) {
+        var_dump($campo);
         $db = new \PDOWrapper\DB;
         
         $campos_string=$this->key_implode($campos,',');
@@ -51,7 +104,7 @@ Class gsbanco {
         //exit;
         $db->bind("cuenta_id",$condicion);
         
-        return($cuentasInactivas = $db->query("SELECT $campos_string FROM movimiento where cuenta_id= :cuenta_id"));
+        return($cuentasInactivas = $db->query("SELECT $campos_string FROM movint where cuenta_id= :cuenta_id"));
         
        
       
@@ -75,11 +128,11 @@ Class gsbanco {
         
         $db = new \PDOWrapper\DB;
         
-        $insert   =  $db->query("INSERT INTO movimiento(monto,tipo_movimiento,fecha_movimiento,descripcion,cuenta_id) VALUES(:monto,:tipo_movimiento,:fecha_movimiento,:descripcion,:cuenta_id)", array("monto"=>$campos["monto"],"tipo_movimiento"=>$campos["tipo_movimiento"],"fecha_movimiento"=>$campos["fecha_movimiento"],"descripcion"=>$campos["descripcion"],"cuenta_id"=>$campos["cuenta_id"]));
+        $insert   =  $db->query("INSERT INTO movint(monto,fecha,descripcion,idcuenta) VALUES(:monto,:fecha_movimiento,:descripcion,:cuenta_id)", array("monto"=>$campos["monto"],"fecha_movimiento"=>$campos["fecha_movimiento"],"descripcion"=>$campos["descripcion"],"cuenta_id"=>$campos["cuenta_id"]));
        if($insert > 0 ) {
         /// se llama al ultimo movimiento ingresado para esta cuenta
            $db->bind("cuenta_id",$campos['cuenta_id']);
-           return($ultimoMovimiento = $db->query("SELECT * FROM movimiento where cuenta_id= :cuenta_id order by id desc limit 1"));
+           return($ultimoMovimiento = $db->query("SELECT * FROM movint where idcuenta= :cuenta_id order by id desc limit 1"));
            
       }
       else{
@@ -98,167 +151,6 @@ Class gsbanco {
         return self::$_instance;
     }
 
-    public function revisarController($idventa) {
-        $bd = Db::getInstance();
-        $tabla = 'venta';
-        $where = 'WHERE identificadorVenta=' . $idventa . ' order by id desc';
-        $limit = ' limit 1';
-        //var_dump($array);
-        $sql = $bd->consultar($tabla, $limit, $where);
-        //var_dump($sql);
-        $stmt = $bd->ejecutar($sql);
-        $cantidad = mysqli_num_rows($stmt);
-        if ($cantidad > 0) {
-            $resultado = mysqli_fetch_array($stmt);
-            if ($resultado['statusVenta'] == 0)
-                $result['resultado'] = 'No procesada';
-            else
-                $result['resultado'] = 'Venta Procesada';
-        }else {
-            $result['resultado'] = 'No hay venta con identificador';
-        }
-
-
-        //var_dump($resultado['statusVenta']);
-        return $result;
-    }
-
-    public function enviarcorreo($array) {
-        // require '../vendor/autoload.php';
-        //error_reporting(E_ALL);
-        error_reporting(E_STRICT);
-
-        date_default_timezone_set('America/Caracas');
-
-        require_once('PHPMailer/class.phpmailer.php');
-        //include("class.smtp.php"); // optional, gets called from within class.phpmailer.php if not already loaded
-
-        $mail = new PHPMailer();
-        
-        ///Cambiar URL base par tomar arhivos para armar el envio de documento.
-        $urlBase='http://tutibiacoin.com/test/';
-
-        $body = file_get_contents($urlBase.'head.html');
-
-        $body = $body . file_get_contents($urlBase.'headcontenido.html');
-        $body = $body . 'Estimado: ' . $array['nombre'] . '<br />';
-        $body = $body . 'Su venta por: ' . $array['cantidad'] . ' Coins Sera atendida a la Brevedad' . '<br />';
-        $body = $body . 'Nombre Chart: ' . $array['nameChart'] . '<br />';
-        $body = $body . 'Pago: ' . $array['cantidadPago'] . ' BsF' . '<br />';
-        $body = $body . 'Identificador de la compra: ' . $array['identificadorVenta'] . ' Con el podra revisar el status de su transferencia' . '<br />';
-        $body = $body . file_get_contents($urlBase.'footercontenido.html');
-        $body = $body . file_get_contents($urlBase.'footer.html');
-        //$body             = preg_replace('/[\]/','',$body);
-        $email->From = "venta@tutibiacoin.com";
-        $email->FromName = "Ventas Team";
-        
-        $mail->IsSMTP(); // telling the class to use SMTP
-        //$mail->Host = "mx1.hostinger.es"; // SMTP server
-        $mail->SMTPAuth = true;
-        //$mail->SMTPSecure = "ssl";
-        $mail->SMTPDebug = 2;
-        $mail->SMTPAuth = true;                  // enable SMTP authentication
-        $mail->Host = "ssl://mx1.hostinger.es"; // sets the SMTP server
-        $mail->Port =465;                    // set the SMTP port for the GMAIL server
-        $mail->Username = "venta@tutibiacoin.com"; // SMTP account username
-        $mail->Password = "02128630183";        // SMTP account password
-
-        $mail->AddAddress($array['email'], $array['nombre']);
-        $mail->AddAddress('rack.stick@gmail.com','Atiende la Venta malparido');
-//$mail->AddReplyTo("name@yourdomain.com","First Last");
-
-        $mail->Subject = "TuTibiaCoin | Venta de Coin Satisfactoria";
-
-        $mail->AltBody = "TuTibiaCoin | Venta de Coin Satisfactoria"; // optional, comment out and test
-
-        $mail->MsgHTML($body);
-
-//$address = "whoto@otherdomain.com";
-        $mail->AddAddress($array['email'], $array['nombre']);
-
-        $mail->IsHTML(true);
-        $mail->Body = $body;
-//$mail->AddAttachment("PHPMailer/test-script/images/phpmailer.gif");      // attachment
-//$mail->AddAttachment("PHPMailer/test-script/images/bkgrnd.gif"); // attachment
-
-        
-        /////Activar esto para poder ver los errores
-        if (!$mail->Send()) {
-            echo "Mailer Error: " . $mail->ErrorInfo;
-        } else {
-            echo "Message sent!";
-        }
-    }
-     public function enviarcorreo1($array) {
-        // require '../vendor/autoload.php';
-        //error_reporting(E_ALL);
-        error_reporting(E_STRICT);
-
-        date_default_timezone_set('America/Caracas');
-
-        require_once('PHPMailer/class.phpmailer.php');
-        //include("class.smtp.php"); // optional, gets called from within class.phpmailer.php if not already loaded
-
-        $mail = new PHPMailer();
-        
-        ///Cambiar URL base par tomar arhivos para armar el envio de documento.
-        $urlBase='http://tutibiacoin.com/test/';
-
-        $body = file_get_contents($urlBase.'head.html');
-
-        $body = $body . file_get_contents($urlBase.'headcontenido.html');
-        $body = $body . 'Estimado: ' . $array['nombre'] . '<br />';
-        $body = $body . 'Su venta por: ' . $array['cantidad'] . ' Coins Sera atendida a la Brevedad' . '<br />';
-        $body = $body . 'Nombre Chart: ' . $array['nameChart'] . '<br />';
-        $body = $body . 'Pago: ' . $array['cantidadPago'] . ' BsF' . '<br />';
-        $body = $body . 'Identificador de la compra: ' . $array['identificadorVenta'] . ' Con el podra revisar el status de su transferencia' . '<br />';
-        $body = $body . file_get_contents($urlBase.'footercontenido.html');
-        $body = $body . file_get_contents($urlBase.'footer.html');
-        //$body             = preg_replace('/[\]/','',$body);
-
-        $mail->IsSMTP(); // telling the class to use SMTP
-        //$mail->Host = "ssl://smtp.gmail.com"; // SMTP server
-        $mail->SMTPDebug =false;                     // enables SMTP debug information (for testing)
-        // 1 = errors and messages
-        // 2 = messages only
-        $mail->SMTPAuth = true;                  // enable SMTP authentication
-        $mail->Host = "ssl://smtp.gmail.com"; // sets the SMTP server
-        $mail->Port = 465;                    // set the SMTP port for the GMAIL server
-        $mail->Username = "tutibiacoin@gmail.com"; // SMTP account username
-        $mail->Password = "tutibiacoin_863";        // SMTP account password
-        //$mail->From     = 'venta@tutibiacoin.com'; 
-        //$mail->FromName = "tutibiacoin.com"; 
-        $mail->AddReplyTo("venta@tutibiacoin.com","tutibiacoin.com"); 
-        //$mail->WordWrap = 50;  
-        $email->From = "venta@tutibiacoin.com";
-        $email->FromName = "Ventas Team TutibiaCoin" ;
-
-        //$mail->AddReplyTo("name@yourdomain.com","First Last");
-
-        $mail->Subject = "TuTibiaCoin | Venta de Coin Satisfactoria";
-
-        $mail->AltBody = "TuTibiaCoin | Venta de Coin Satisfactoria"; // optional, comment out and test
-
-        $mail->MsgHTML($body);
-
-//$address = "whoto@otherdomain.com";
-        // $mail->AddAddress($array['email'], $array['nombre'],'rack.stick@gmail.com','Atiende la Venta malparido');
-        $mail->AddAddress($array['email'], $array['nombre']);
-        $mail->AddAddress('rack.stick@gmail.com','Atiende la Venta malparido');
-        //$mail->AddAddress('tutibiacoin@gmail.com',$array['nameChart']);
-        $mail->IsHTML(true);
-        $mail->Body = $body;
-//$mail->AddAttachment("PHPMailer/test-script/images/phpmailer.gif");      // attachment
-//$mail->AddAttachment("PHPMailer/test-script/images/bkgrnd.gif"); // attachment
-
-        
-//        /////Activar esto para poder ver los errores
-        
-        if (!$mail->Send()) {
-           // echo "Mailer Error: " . $mail->ErrorInfo;
-        } else {
-            //echo "Message sent!";
-        }
-    }
-
+    
+    
 }
